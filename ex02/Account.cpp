9b1460868895cb163ab6e25ad7fe069046ddf7ec
Kelly Brener-Raffalli <kbrener-@c3r1p4.42perpignan.fr>
2024-10-16 16:15:33 +0200

    withdrawals

---
+++ ex02/Account.cpp
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 14:40:15 by kbrener-          #+#    #+#             */
+/*   Updated: 2024/10/16 16:15:12 by kbrener-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <cstdio>
+#include <limits>
+#include <ctime>
+
+int	Account::_nbAccounts = 0; // Initialisation à 0
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+void	Account::_displayTimestamp(void) {
+	std::time_t	now = std::time(0);
+	std::cout<<"["<<std::ctime(&now)<<"] ";
+}
+
+Account::Account(int initial_deposit) {
+	this->_accountIndex = _nbAccounts++;
+	this->_amount = initial_deposit;
+	_totalAmount =+ this->_amount;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout<<"index:"<<this->_accountIndex<<";amount:"<<this->_amount\
+	<<";created"<<std::endl;
+}
+
+void	Account::displayStatus( void ) const {
+	_displayTimestamp();
+	std::cout<<"index:"<<this->_accountIndex<<";amount:"<<this->_amount\
+	<<";deposits:"<<this->_nbDeposits<<";withdrawals:"<<this->_nbWithdrawals<<std::endl;
+}
+
+void	Account::displayAccountsInfos( void ) {
+	_displayTimestamp;
+	std::cout<<"accounts:"<<_nbAccounts<<";total:"<<_totalAmount<<";deposits:"\
+	<<_totalNbDeposits<<";withdrawals:"<<_totalNbWithdrawals<< std::endl;
+}
+
+void	Account::makeDeposit(int deposit) {
+	_displayTimestamp();
+	std::cout<<"index:"<<this->_accountIndex<<";p_amount:"<<this->_amount<<\
+	";deposit:"<<deposit;
+	this->_amount =+ deposit;
+	this->_nbDeposits++;
+	_totalNbDeposits++;
+	_totalAmount =+ deposit;
+	std::cout<<";amount:"<<this->_amount<<";nb_deposits:"<<this->_nbDeposits\
+	<<std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal) {
+	_displayTimestamp();
+	std::cout<<"index:"<<this->_accountIndex<<";p_amount:"<<this->_amount<<\
+	";withdrawal:";
+	bool	wd_allowed;
+	if (withdrawal <= this->_amount)
+		wd_allowed = true;
+	else {
+		wd_allowed = false;
+		std::cout<<"refused"<<std::endl;
+		return (wd_allowed);
+	}
+	this->_amount =- withdrawal;
+	this->_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	_totalAmount =- withdrawal;
+	std::cout<<withdrawal<<";amount:"<<this->_amount<<";nb_withdrawals:"<<this->_nbWithdrawals\
+	<<std::endl;
+}
--- ex02/tests.cpp
+++ ex02/tests.cpp
@@ -19,38 +19,38 @@ int		main( void ) {
 	typedef std::vector<int>								  ints_t;//cree un alias pour creer un tableau de int
 	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;//cree un alias pour creer une pair d'iterateur
 
-	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
-	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
-	accounts_t				accounts( amounts, amounts + amounts_size );
-	accounts_t::iterator	acc_begin	= accounts.begin();
-	accounts_t::iterator	acc_end		= accounts.end();
+	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };//cree un tableau de int ou chaque int sera le depot de depart d'un Account
+	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );//calcul la taille du tableau de int
+	accounts_t				accounts( amounts, amounts + amounts_size );//cree les comptes avec des depots de depart dans un vecteur de class Account;
+	accounts_t::iterator	acc_begin	= accounts.begin();//determine l'iterateur de debut du vecteur
+	accounts_t::iterator	acc_end		= accounts.end();//determine l'iterateur de fin du vecteur
 
-	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
-	size_t const		d_size( sizeof(d) / sizeof(int) );
-	ints_t				deposits( d, d + d_size );
-	ints_t::iterator	dep_begin	= deposits.begin();
-	ints_t::iterator	dep_end		= deposits.end();
+	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };//cree un tableau de int dont chaque int sera un depot pour le Account correspondant au meme rang
+	size_t const		d_size( sizeof(d) / sizeof(int) );//taille du tableau de int
+	ints_t				deposits( d, d + d_size );//cree un vecteur de int du 1er depot au dernier
+	ints_t::iterator	dep_begin	= deposits.begin();//determine l'iterateur de debut de vecteur
+	ints_t::iterator	dep_end		= deposits.end();//determine l'iterateur de fin de vecteur
 
-	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
-	size_t const		w_size( sizeof(w) / sizeof(int) );
-	ints_t				withdrawals( w, w + w_size );
-	ints_t::iterator	wit_begin	= withdrawals.begin();
-	ints_t::iterator	wit_end		= withdrawals.end();
+	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };//cree un tableau de int dont chaque int sera un retrait
+	size_t const		w_size( sizeof(w) / sizeof(int) );//taille du tableau de int
+	ints_t				withdrawals( w, w + w_size );//cree un vecteur de int pour les retraits
+	ints_t::iterator	wit_begin	= withdrawals.begin();//determine l'iterateur de debut de vecteur
+	ints_t::iterator	wit_end		= withdrawals.end();//determine l'iterateur de fin de vecteur
 
-	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	Account::displayAccountsInfos();//print les infos de l'ensemble des comptes
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );//appelle la fonction displayStatus pour chaque instance
 
 	for ( acc_int_t it( acc_begin, dep_begin );
 		  it.first != acc_end && it.second != dep_end;
 		  ++(it.first), ++(it.second) ) {
 
-		(*(it.first)).makeDeposit( *(it.second) );
+		(*(it.first)).makeDeposit( *(it.second) );//tant qu'on n'a pas atteint la fin du vecteur account ou deposit, on fait le depot i sur l'account i
 	}
 
 	Account::displayAccountsInfos();
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
-	for ( acc_int_t it( acc_begin, wit_begin );
+	for ( acc_int_t it( acc_begin, wit_begin );//cree une paire d'iterateur avec le 1er iterateur de Account et le 1er iterateur de retrait
 		  it.first != acc_end && it.second != wit_end;
 		  ++(it.first), ++(it.second) ) {
 
